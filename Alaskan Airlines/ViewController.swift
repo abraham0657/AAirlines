8731d10d35700401e1ea9d7f8321ea3cb69929b5
Abraham Weldu <abe973t@gmail.com>
2017-10-31 19:14:05 -0500

    added validation for textfields

--- Alaskan Airlines/ViewController.swift
+++ Alaskan Airlines/ViewController.swift
@@ -26,7 +26,36 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    
+    @IBAction func searchBtnPressed(_ sender: UIButton) {
+        if airportTextField.text?.count != 3 {
+            // error
+        } else if minBeforeTextField.text!.isEmpty || minAfterTextField.text!.isEmpty {
+            // error
+        } else {
+            // conduct search
+        }
+    }
 
 }
 
+extension ViewController: UITextFieldDelegate {
+    func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
+        if textField == airportTextField {
+            if string != "" && airportTextField.text!.count >= 3 {
+                return false
+            } else {
+                airportTextField.text = (textField.text! as NSString).replacingCharacters(in: range, with: string.uppercased())
+                return false    // returning since we already updated textfield
+            }
+        }
+        
+        return true
+    }
+
+    
+    func textFieldDidEndEditing(_ textField: UITextField) {
+        let skyTxtField = textField as! SkyFloatingLabelTextField
+        skyTxtField.errorMessage = (textField.text?.isEmpty)! ? "Invalid Input" : nil
+    }
+}
